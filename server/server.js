4f28298de3573717c11331bfceaeaad8890b03ec
Salih Alperen <sal.alperen1@gmail.com>
2023-08-21 22:09:53 +0200

    Add initial directories and libraries

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import morgan from "morgan";
+const app = express();
+
+// middlewares
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.get("/", (req, res) => {
+  console.log("Hi there");
+  res.send("abc");
+});
+
+// routes
+
+// errrors
+
+// start
+
+const PORT = process.env.port || 5000;
+
+app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
